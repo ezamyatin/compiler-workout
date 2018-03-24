10ba9a1a2ec1abca7fb56f3727138ce0f734c3e9
evgeny <evgeny@jetlore.com>
2018-03-24 12:03:58 +0300

    HW3 complete

--- src/Language.ml
+++ src/Language.ml
@@ -2,7 +2,8 @@
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
 open GT
-
+open List
+open Ostap
 (* Opening a library for combinator-based syntax analysis *)
 open Ostap.Combinators
        
@@ -44,7 +45,31 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+
+    let toBool x = x <> 0
+    let toInt x = if x then 1 else 0
+
+    let rec eval state expr = match expr with
+    	| Const value -> value
+    	| Var name -> state name
+    	| Binop(op, l, r) ->
+    		let (x, y) = (eval state l, eval state r) in
+    		match op with
+    			| "!!" -> toInt (toBool x || toBool y)
+	        	| "&&" -> toInt (toBool x && toBool y)
+          		| "==" -> toInt (x == y)
+          		| "!=" -> toInt (x <> y)
+	          	| "<=" -> toInt (x <= y)
+          		| "<"  -> toInt (x < y)
+          		| ">=" -> toInt (x >= y)
+    	      	| ">"  -> toInt (x > y)
+	          	| "+"  -> x + y
+          		| "-"  -> x - y
+          		| "*"  -> x * y
+        	  	| "/"  -> x / y
+    	      	| "%"  -> x mod y
+	          	| _    -> failwith (Printf.sprintf "Unsupported binary operator %s" op);;
+
 
     (* Expression parser. You can use the following terminals:
 
@@ -52,11 +77,25 @@ module Expr =
          DECIMAL --- a decimal constant [0-9]+ as a string
    
     *)
-    ostap (
-      parse: empty {failwith "Not implemented yet"}
-    )
+    let identity x = x
+    let make_operation operation = (ostap ($(operation)), fun x y -> Binop (operation, x, y))
 
-  end
+    ostap (
+      	parse: expression;
+      	expression:
+    		!(Util.expr identity
+          	[|
+            	`Lefta, [make_operation "!!"];
+            	`Lefta, [make_operation "&&"];
+            	`Nona,  [make_operation "<="; make_operation ">="; make_operation "<"; make_operation ">"; make_operation "=="; make_operation "!="];
+            	`Lefta, [make_operation "+"; make_operation "-"];
+            	`Lefta, [make_operation "*"; make_operation "/"; make_operation "%"]
+          	|]
+          	operations
+     	);
+     	operations: x:IDENT {Var x} | n:DECIMAL {Const n} | -"(" expression -")"
+    )
+   end
                     
 (* Simple statements: syntax and sematics *)
 module Stmt =
@@ -78,11 +117,22 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval (s, i, o) expression = match expression with
+    	| Read x           -> let (h :: rest) = i in (Expr.update x h s, rest, o)
+		| Write e          -> (s, i, o @ [(Expr.eval s e)])
+		| Assign (x, expr) -> (Expr.update x (Expr.eval s expr) s, i, o)
+		| Seq (s_, t_)     -> eval (eval (s, i, o) s_) t_
+		| _                -> failwith (Printf.sprintf "Unsupported expression")
+ 
 
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not implemented yet"}
+    	parse: statements;
+		statements: <s1::s2> : !(Util.listBy)[ostap (";")][stmt] {List.fold_left (fun x y -> Seq (x, y)) s1 s2};
+		stmt:
+			x:IDENT ":=" e:!(Expr.parse) {Assign (x, e)}
+				| "write" "(" e:!(Expr.parse) ")" {Write e}
+				| "read" "(" x:IDENT ")" {Read x}
     )
       
   end
